f9de74e8e81460714e380ebb42dad985d220f531
Baptiste Truchet <baptiste.truchet@gmail.com>
2024-02-02 15:38:04 +0100

    Fix spec

--- __tests__/setup.ts
+++ __tests__/setup.ts
@@ -2,6 +2,7 @@ import { afterAll, afterEach, beforeAll } from "vitest";
 import { cleanup } from "@testing-library/react";
 import { server } from "@mocks/server";
 import "@testing-library/jest-dom";
+import "@mocks/window/match-media";
 
 beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
 afterEach(() => {
